ba682841c861c6aa6fa454bb03130810252954f0
AlessandraSousa <105075066+AlessandraSousa@users.noreply.github.com>
2022-05-28 21:25:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var Your_comments;
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+Your_comments = [];
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  Your_comments.unshift(getNumberOrString(document.getElementById('text').value));
+  let element_list = document.getElementById('list');
+  let new_ul = document.createElement('ul');
+  new_ul.innerText = getNumberOrString(document.getElementById('text').value);
+
+  element_list.appendChild(new_ul);
+
+});
